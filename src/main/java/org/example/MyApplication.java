f1d88276bd1c18c96555bcfbb21cffd8c0d74e18
Ferid Ramadani <f.ramadani.f@gmail.com>
2023-12-05 12:20:02 +0100

    Revert "initial"
    This reverts commit 824099ab3c96e9ef9ff08eef86be3f28bcb5709c.

--- src/main/java/org/example/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class MyApplication extends ResourceConfig {
-    public MyApplication() {
-        packages("org.example"); // Change this to the package where your resources are located
-    }
-}
